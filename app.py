ad2af9062d4909ebeff29007d49680f7fea301ed
Toadman0 <7romanu@gmail.com>
2024-04-24 23:39:17 +0300

    Update 24.04.2024 23:39

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, session
+from data import db_session
+from data.users import User
 import requests
 
 language_family = {
@@ -147,7 +149,9 @@ def get_transcription(original, word, lang_tr, family, lang_1='en'):
             return '—'
 
 
+db_session.global_init("db/users.db")
 app = Flask(__name__)
+app.secret_key = b'_6#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -161,6 +165,12 @@ def main_page():
 
         lang_1 = 'en'
         original = responce
+
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.username == session['username'])
+        print(user)
+        user.name = "Измененное имя пользователя"
+        db_sess.commit()
 
         orig_w = f'{lang_1}: {original} [{get_transcription(original, original, lang_1, family, lang_1)}]'
         if get_translated_word(original, family, lang_1) == 'your word is not in dictionary':
@@ -180,11 +190,14 @@ def sign_in():
         # responce равен вводу пользователя
         password = request.form['password']
         username = request.form['username']
-        if password != 'a' or username != 'a':
+        db_sess = db_session.create_session()
+        users = db_sess.query(User).filter((User.username == username) & (User.password == password))
+        if not users:
             error = u'error'
         else:
-            flash(u'sakses')
-            return redirect(url_for(''))
+            session['username'] = username
+            session['password'] = password
+            return redirect(url_for('main_page'))
         print(password, username)
     return render_template('test.html', error=error)
 
@@ -198,8 +211,16 @@ def sign_up():
         # responce равен вводу пользователя
         password = request.form['password']
         username = request.form['username']
+        session['username'] = username
+        session['password'] = password
+        user = User()
+        user.username = username
+        user.password = password
+        db_sess = db_session.create_session()
+        db_sess.add(user)
+        db_sess.commit()
         print(password, username)
-
+        return redirect(url_for('main_page'))
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
